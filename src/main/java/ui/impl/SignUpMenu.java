f0a9e71d14fca1809aff96e843dfc5ad30b38615
Cheesy2137 <adrian997112999@gmail.com>
2026-03-18 02:47:35 +0100

    Add menu and logic

--- src/main/java/ui/impl/SignUpMenu.java
+++ src/main/java/ui/impl/SignUpMenu.java
@@ -1,9 +1,12 @@
 package ui.impl;
 
 import config.ApplicationContext;
+import model.impl.DefaultUser;
 import service.UserManagementService;
 import service.impl.DefaultUserManagementService;
 import ui.Menu;
+
+import java.util.Scanner;
 
 public class SignUpMenu implements Menu {
     private UserManagementService userManagementService;
@@ -16,12 +19,33 @@ public class SignUpMenu implements Menu {
 
     @Override
     public void start() {
-        // <write your code here>
+        Scanner scanner = new Scanner(System.in);
+
+        printMenuHeader();
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter your second name: ");
+        String secondName = scanner.nextLine();
+        System.out.print("Enter your e-mail: ");
+        String email = scanner.nextLine();
+        System.out.print("Enter your password: ");
+        String password = scanner.nextLine();
+
+        DefaultUser user = new DefaultUser(name, secondName, password, email);
+        String errorMessage = userManagementService.registerUser(user);
+
+        if (errorMessage.isEmpty()) {
+            context.setLoggedInUser(user);
+            System.out.println("You have successfully registered!");
+        } else {
+            System.out.println(errorMessage);
+        }
+
     }
 
     @Override
     public void printMenuHeader() {
-        // <write your code here>
+        System.out.println("---SIGN-UP-MENU---");
     }
 
 }
