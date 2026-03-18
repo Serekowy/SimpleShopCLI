bd8055e461d1de09b47c4815832790972b7e8e1d
Cheesy2137 <adrian997112999@gmail.com>
2026-03-18 02:47:13 +0100

    Add menu and logic

--- src/main/java/ui/impl/MainMenu.java
+++ src/main/java/ui/impl/MainMenu.java
@@ -3,6 +3,8 @@ package ui.impl;
 import config.ApplicationContext;
 import ui.Menu;
 
+import java.util.Scanner;
+
 public class MainMenu implements Menu {
 
     public static final String MENU_COMMAND = "menu";
@@ -11,13 +13,13 @@ public class MainMenu implements Menu {
             + "1. Sign Up" + System.lineSeparator() + "2. Sign In"
             + System.lineSeparator() + "3. Product Catalog" + System.lineSeparator()
             + "4. My Orders" + System.lineSeparator() + "5. Settings" + System.lineSeparator() +
-            "6. Customer List";
+            "6. Customer List" + System.lineSeparator();
 
     private static final String MAIN_MENU_TEXT_FOR_LOGGED_IN_USER = "Please, enter number in console to proceed." + System.lineSeparator()
             + "1. Sign Up" + System.lineSeparator() + "2. Sign Out"
             + System.lineSeparator() + "3. Product Catalog" + System.lineSeparator()
             + "4. My Orders" + System.lineSeparator() + "5. Settings" + System.lineSeparator() +
-            "6. Customer List";;
+            "6. Customer List" + System.lineSeparator();
 
     private ApplicationContext context;
 
@@ -27,13 +29,55 @@ public class MainMenu implements Menu {
 
     @Override
     public void start() {
-        // <write your code here>
+        if (context == null) context.setMainMenu(this);
+
+        boolean running = true;
+        Menu choosenMenu = null;
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+
+            printMenuHeader();
+            String userInput = scanner.nextLine();
+            if (MENU_COMMAND.equalsIgnoreCase(userInput)) break;
+
+            switch (userInput) {
+                case "1":
+                    choosenMenu = new SignUpMenu();
+                    break;
+                case "2":
+                    if (context.getLoggedInUser() == null) choosenMenu = new SignInMenu();
+                    else choosenMenu = new SignOutMenu();
+                    break;
+                case "3":
+                    choosenMenu = new ProductCatalogMenu();
+                    break;
+                case "4":
+                    choosenMenu = new MyOrdersMenu();
+                    break;
+                case "5":
+                    choosenMenu = new SettingsMenu();
+                    break;
+                case "6":
+                    choosenMenu = new CustomerListMenu();
+                    break;
+                default:
+                    System.out.println("Invalid input, choose option 1-5. Please try again.");
+                    continue;
+            }
+            choosenMenu.start();
+        }
 
     }
 
     @Override
     public void printMenuHeader() {
-        // <write your code here>
-
+        System.out.println("---MAIN-MENU---");
+        if (context.getLoggedInUser() == null) {
+            System.out.print(MAIN_MENU_TEXT_FOR_LOGGED_OUT_USER);
+        } else {
+            System.out.print(MAIN_MENU_TEXT_FOR_LOGGED_IN_USER);
+        }
+        System.out.print("Input: ");
     }
 }
