4c7c5553d106d184e3c8de0c759d4d1ade3c5081
Cheesy2137 <adrian997112999@gmail.com>
2026-03-20 18:55:01 +0100

    Add logic to methods and fields

--- src/main/java/model/impl/DefaultCart.java
+++ src/main/java/model/impl/DefaultCart.java
@@ -3,30 +3,44 @@ package model.impl;
 import model.Cart;
 import model.Product;
 
+import java.util.Arrays;
+
 public class DefaultCart implements Cart {
-    // <write your code here>
+
+    private final int AMOUNT_OF_PRODUCTS = 10;
+    private int nextIndex = 0;
+    private Product[] products;
+
+    {
+        products = new Product[AMOUNT_OF_PRODUCTS];
+    }
 
     @Override
     public boolean isEmpty() {
-        // <write your code here>
-
-        return true;
+        return nextIndex == 0;
     }
 
     @Override
     public void addProduct(Product product) {
-        // <write your code here>
+        if(product == null) return;
+
+        if(products.length >= nextIndex) {
+            products = Arrays.copyOf(products, AMOUNT_OF_PRODUCTS << 1);
+        }
+
+        nextIndex += 1;
+        products[nextIndex] = product;
+
     }
 
     @Override
     public Product[] getProducts() {
-        // <write your code here>
-
-        return null;
+        return Arrays.copyOf(products, nextIndex);
     }
 
     @Override
     public void clear() {
-        // <write your code here>
+        nextIndex = 0;
+        products = new Product[AMOUNT_OF_PRODUCTS];
     }
 }
