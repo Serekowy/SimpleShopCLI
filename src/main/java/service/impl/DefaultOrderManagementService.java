9b0d987a5afcc4b96c20e52a5e4c0b457942d648
Cheesy2137 <adrian997112999@gmail.com>
2026-03-21 18:27:26 +0100

    Add logic to methods and new fields

--- src/main/java/service/impl/DefaultOrderManagementService.java
+++ src/main/java/service/impl/DefaultOrderManagementService.java
@@ -1,15 +1,17 @@
 package service.impl;
 
 import model.Order;
+import model.impl.DefaultOrder;
 import service.OrderManagementService;
+
+import java.util.Arrays;
 
 public class DefaultOrderManagementService implements OrderManagementService {
 
     private static final int DEFAULT_ORDER_CAPACITY = 10;
-
     private static DefaultOrderManagementService instance;
-
-    // <write your code here>
+    private Order[] orders;
+    private int nextId = 0;
 
     public static OrderManagementService getInstance() {
         if (instance == null) {
@@ -18,25 +20,43 @@ public class DefaultOrderManagementService implements OrderManagementService {
         return instance;
     }
 
+    {
+        orders = new DefaultOrder[DEFAULT_ORDER_CAPACITY];
+    }
+
     @Override
     public void addOrder(Order order) {
-        // <write your code here>
+        if(nextId >= orders.length) orders = Arrays.copyOf(orders, orders.length << 1);
+
+        orders[nextId++] = order;
     }
 
     @Override
     public Order[] getOrdersByUserId(int userId) {
-        // <write your code here>
-        return null;
+        if(orders[0] == null) return null;
+
+        Order[] userOrders = new Order[orders.length];
+        int i = 0;
+
+        for (Order order : orders) {
+            if (order == null) {
+                break;
+            } else if (order.getCustomerId() == userId) {
+                userOrders[i++] = order;
+            }
+        }
+
+        return Arrays.copyOf(userOrders, i);
     }
 
     @Override
     public Order[] getOrders() {
-        // <write your code here>
-        return null;
+        return orders;
     }
 
     void clearServiceState() {
-        // <write your code here>
+        nextId = 0;
+        orders = new DefaultOrder[DEFAULT_ORDER_CAPACITY];
     }
 
 }
