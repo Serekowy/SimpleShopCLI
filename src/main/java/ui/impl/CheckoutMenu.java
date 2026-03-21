28f87bc98a5cef3c4d1b8987b0c3d657c1671e07
Cheesy2137 <adrian997112999@gmail.com>
2026-03-21 18:25:54 +0100

    Add new methods and logic

--- src/main/java/ui/impl/CheckoutMenu.java
+++ src/main/java/ui/impl/CheckoutMenu.java
@@ -1,11 +1,17 @@
 package ui.impl;
 
 import config.ApplicationContext;
+import model.Order;
+import model.impl.DefaultOrder;
 import service.OrderManagementService;
 import service.impl.DefaultOrderManagementService;
 import ui.Menu;
 
+import java.util.Scanner;
+
 public class CheckoutMenu implements Menu {
+    Scanner scanner = new Scanner(System.in);
+
     private ApplicationContext context;
     private OrderManagementService orderManagementService;
 
@@ -16,11 +22,44 @@ public class CheckoutMenu implements Menu {
 
     @Override
     public void start() {
-        // <write your code here>
+
+        while (true) {
+            printMenuHeader();
+
+            if (!createOrder(getUserCardNumber())) continue;
+
+            context.getSessionCart().clear();
+            break;
+        }
+
+        System.out.println("Thanks a lot for your purchase. Details about order delivery are sent to your email.");
+    }
+
+
+    private boolean createOrder(String cardNumber) {
+        Order order = new DefaultOrder();
+
+        if(!order.isCreditCardNumberValid(cardNumber)) {
+            System.out.println("Invalid card number!");
+            return false;
+        }
+
+        order.setCreditCardNumber(cardNumber);
+        order.setCustomerId(context.getLoggedInUser().getId());
+        order.setProducts(context.getSessionCart().getProducts());
+
+        orderManagementService.addOrder(order);
+
+        return true;
+    }
+
+    private String getUserCardNumber() {
+        return scanner.nextLine();
     }
 
     @Override
     public void printMenuHeader() {
-        // <write your code here>
+        System.out.println("---CHECKOUT-MENU---");
+        System.out.print("Enter your credit card number without spaces and press enter if you confirm purchase: ");
     }
 }
